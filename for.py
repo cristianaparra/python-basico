a7d85025568bd077232cfdc7bea51568716a5060
Cristian Parra <cristian.a.parra@gmail.com>
2020-09-21 23:53:21 -0300

    creamos ciclos for, while, reccorrimos strinmg y usamos break y continue

---
+++ for.py
@@ -0,0 +1,13 @@
+# contador = 1
+# print(contador)
+# while contador < 1000000000000000000000000:
+#     contador = contador + 1
+#     print(contador)
+
+
+for contador in range(1, 1001):
+    print(contador)
+
+# para la variable i en el rango quer va del 0 al 9 se va a multiplicar x 11
+for i in range(0, 10):
+    print(11 * i)
