4f81f483b434134f1b09d16a35ec7e358ad40f08
Cristian Parra <cristian.a.parra@gmail.com>
2020-10-26 00:52:10 -0300

    implementado get post delete

--- TODOflaskpython/src/app.py
+++ TODOflaskpython/src/app.py
@@ -1,11 +1,36 @@
-from flask import Flask
-
+from flask import Flask, jsonify, request
+import json
 
 app = Flask(__name__)
 
-@app.route('/cristian', methods=['GET'])
+
+todos = [
+    { "label": "My first task", "done": False },
+    { "label": "My second task", "done": False }
+]
+
+
+@app.route('/todos', methods=['GET'])
 def hello_world():
-    return 'hola tonto'
+    return jsonify(todos)
+
+@app.route('/todos', methods=['POST'])
+def add_new_todo():
+    request_body = request.data
+    decoded_object = json.loads(request_body)
+    todos.append(decoded_object)
+    print("Incoming request with the following body", request_body)
+    return jsonify(todos)
+
+@app.route('/todos/<int:position>', methods=['DELETE'])
+def delete_todo(position):
+    print("This is the position to delete: ",position)
+    todos.pop(position)
+    return jsonify(todos)
+
+
+
+
 
 if __name__ == '__main__':
   app.run(host='0.0.0.0', port=3245, debug=True)
